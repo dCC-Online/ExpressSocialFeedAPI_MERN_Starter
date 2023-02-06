042910b8fe404535e873222e9134c8e92bac80c6
Daniel Tulpa <dtulpa16@yahoo.com>
2023-02-06 13:42:01 -0600

    Added Postman Collection + Change Port To 5000

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const social = require("./routes/social")
 app.use(express.json());
 app.use('/api/social', social)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
